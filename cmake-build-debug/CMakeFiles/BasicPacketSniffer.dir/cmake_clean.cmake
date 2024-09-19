5e0107f93448e44428a211c3135eb9a9aa0c3b43
TheArinav <88740085+TheArinav@users.noreply.github.com>
2024-09-19 10:02:50 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/BasicPacketSniffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BasicPacketSniffer"
+  "BasicPacketSniffer.pdb"
+  "CMakeFiles/BasicPacketSniffer.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicPacketSniffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
